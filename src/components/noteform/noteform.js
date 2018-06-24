eb215cadd7a457e3a4d0e4ea37887d09aae66672
theidi267 <timea.heidenreich@gmail.com>
2018-06-23 20:52:46 -0700

    added some logic, deployed to heroku

--- src/components/noteform/noteform.js
+++ src/components/noteform/noteform.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Dashboard from '../dashboard/dashboard.js';
+import App from '../app.js';
 
-let emptyNote = {
-  title: '',
-  content: '',
-};
 
 export default class Noteform extends React.Component {
+
   constructor(props){
-    super(props)
-  };
+    super(props);
+  }
 
-  this.state =
+  componentDidUpdate() {
+    console.log('__STATE__', this.state);
+  }
 
-
+  render(){
+    return (
+      <form>
+        <h2>Create A New Note:</h2>
+        <div>
+          <label>Title:</label>
+          <input type="text" name="title" />
+        </div>
+        <div>
+          <label>Content:</label>
+          <textarea name="content" rows="4" cols="50" />
+        </div>
+        <div>
+          <input type="submit" value="Submit" onChange={App.addItem({"title":"testing"})}/>
+        </div>
+      </form>
+    );
+  }
 }
